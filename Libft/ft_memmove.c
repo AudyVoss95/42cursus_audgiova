6996189bf6acaa8e11ac44d43e9b534d3be70b90
Audy <audgiova@student.42sp.org.br>
2026-06-05 18:23:05 -0300

    Libft_ft_memmove

---
+++ Libft/ft_memmove.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: audgiova <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/05 18:06:39 by audgiova          #+#    #+#             */
+/*   Updated: 2026/06/05 18:06:41 by audgiova         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+void	*memmove(void *dest, const void *src, int n);
+
+void	*memmove(void *dest, const void *src, int n)
+{
+	unsigned char *ptr_dest;
+	const unsigned char *ptr_src;
+	int i;
+
+	ptr_dest = (unsigned char *)dest;
+	ptr_src = (const unsigned char *)src;
+	i = 0;
+	if(!dest && !src)
+	{
+		return (ptr_dest);
+	}
+
+	if(dest > src)
+	{
+		ft_memcpy(ptr_dest, ptr_src, n);
+	}
+	else
+	{
+		while (i < n)
+		{
+			ptr_dest[i] = ptr_src[i];
+			i++;
+		}
+	}
+	return(ptr_dest);
+
+
+}
