fb35605640800c41f3088192a7bf709c488643b1
Audy <audgiova@student.42sp.org.br>
2026-06-05 17:44:27 -0300

    Libft_ft_bzero

--- Libft/ft_bzero.c
+++ Libft/ft_bzero.c
@@ -1,25 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: audgiova <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/05 16:25:00 by audgiova          #+#    #+#             */
-/*   Updated: 2026/06/05 16:25:09 by audgiova         ###   ########.fr       */
+/*   Created: 2026/06/05 17:36:03 by audgiova          #+#    #+#             */
+/*   Updated: 2026/06/05 17:36:04 by audgiova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-#define LIBFT_H
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint (int c);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
-int	ft_strlen(char *str);
-void	*ft_memset(void *s, int c, int n);
 void	bzero(void *s, int n);
-#endif // DEBUG
+
+
+void	bzero(void *s, int n)
+{
+	unsigned char *pointer;
+
+	pointer = (unsigned char *)s;
+
+	while (n > 0)
+	{
+		pointer[n] = '\0';
+		n--;
+	}
+}
