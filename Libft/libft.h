9ae99fba288d5a8408ea6f063b0bfee1c3f23be9
Audy <audgiova@student.42sp.org.br>
2026-06-05 16:51:27 -0300

    Adiciona arquivos da Libft diretamente no projeto

---
+++ Libft/libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: audgiova <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/05 16:25:00 by audgiova          #+#    #+#             */
+/*   Updated: 2026/06/05 16:25:09 by audgiova         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifdef LIBFT_H
+#define LIBFT_H
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint (int c);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+int	ft_strlen(char *str);
+#endif // DEBUG
