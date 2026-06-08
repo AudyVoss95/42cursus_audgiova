f340245f37fd7efed11bb7a76d9b21dd208be26e
Audy <audgiova@student.42sp.org.br>
2026-06-08 16:41:04 -0300

    ft_memchr

---
+++ Libft/ft_memchr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: audgiova <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/08 16:19:44 by audgiova          #+#    #+#             */
+/*   Updated: 2026/06/08 16:19:48 by audgiova         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+void	*ft_memchr(const void *s, int c, size_t n);
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char *ptr;
+	int i;
+
+	i = 0;
+	ptr = (unsigned char *)s;
+	while (*ptr && i < n)
+	{
+		if(*ptr == (char)c)
+		{
+			return ((void *)(ptr + i));
+		}
+		ptr++;
+		i++;
+	}
+	return( '\0');
+	
+}
--- Libft/libft.h
+++ Libft/libft.h
@@ -30,4 +30,5 @@ void	*ft_memmove(void *dest, const void *src, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int	strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 #endif // DEBUG
