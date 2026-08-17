ff2ef9e40e58dbe11f9dd9c3ec0504ec22d35fd7
Audy <audgiova@student.42sp.org.br>
2026-08-17 17:51:26 -0300

    initial commit gnl

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: audgiova <audgiova@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/17 16:42:17 by audgiova          #+#    #+#             */
+/*   Updated: 2026/08/17 17:26:52 by audgiova         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE = 42
+# endif // !BUFFER_SIZE
+
+char	*get_next_line(int fd);
+
+#endif // !GET_NEXT_LINE
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: audgiova <audgiova@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/17 17:33:06 by audgiova          #+#    #+#             */
+/*   Updated: 2026/08/17 17:49:49 by audgiova         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (i <= ft_strlen((char *)s))
+	{
+		if (s[i] == (char) c)
+		{
+			return ((char *) &s[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char		*pointer_dest;
+	const unsigned char	*pointer_src;
+	size_t				i;
+
+	pointer_dest = (unsigned char *)dest;
+	pointer_src = (const unsigned char *)src;
+	if (!dest && !src)
+	{
+		return (dest);
+	}
+	i = 0;
+	while (i < n)
+	{
+		pointer_dest[i] = pointer_src[i];
+		i++;
+	}
+	pointer_dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len_s1;
+	size_t	len_s2;
+	char	*buffer;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len_s1 = ft_strlen((char *)s1);
+	len_s2 = ft_strlen((char *)s2);
+	buffer = malloc(len_s1 + len_s2 + 1);
+	ft_memcpy(buffer, s1, len_s1);
+	ft_memcpy(buffer + len_s1, s2, len_s2);
+	free(s1);
+	return (buffer);
+}
