8f1a4e387ab6b4fe851edd224ca49b13a727c53c
Audy <audgiova@student.42sp.org.br>
2026-08-21 16:02:41 -0300

    gnl.c fixes

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,13 +6,82 @@
 /*   By: audgiova <audgiova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/17 17:27:42 by audgiova          #+#    #+#             */
-/*   Updated: 2026/08/21 14:57:09 by audgiova         ###   ########.fr       */
+/*   Updated: 2026/08/21 16:02:22 by audgiova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_extract_line(char *storage)
+{
+	char	*buffer;
+	char	*temp;
+	int		len;
+	int		i;
+
+	if (!storage || !storage[0])
+		return (NULL);
+	temp = (char *)storage;
+	len = 0;
+	while (storage[len] && storage[len] != '\n')
+	{
+		len++;
+	}
+	if (storage[len] == '\n')
+		buffer = (char *)malloc(sizeof(char) * (len + 2));
+	else
+		buffer = (char *)malloc(sizeof(char) * (len + 1));
+	i = 0;
+	while (storage[i])
+	{
+		buffer[i] = storage[i];
+		i++;
+	}
+	buffer[i] = '\0';
+	return (buffer);
+}
+
+void	ft_clean_storage(char *storage)
+{
+	int		i;
+	int		j;
+	char	*new_str;
+
+	i = 0;
+	while (storage[i] && storage[i] != '\n')
+	{
+		i++;
+	}
+	if (storage[i] == '\0')
+		free(storage);
+		return (NULL);
+	j = 0;
+	while (storage[i] && storage[i] != '\n')
+	{
+		new_str[j] = storage[i];
+		j++;
+		i++;
+	}
+	free(storage);
+	return (new_str);
+}
+
 char	*get_next_line(int fd)
 {
-	
+	char	*storage;
+	size_t	bytes_read;
+	char	*buffer;
+
+	while (!ft_strchr(storage, '\n') && bytes_read != 0)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(bytes_read);
+			return (NULL);
+		}
+		buffer[bytes_read] = '\0';
+		storage = ft_strjoin(storage, buffer);
+	}
+	return (storage);
 }
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: audgiova <audgiova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/17 17:33:06 by audgiova          #+#    #+#             */
-/*   Updated: 2026/08/21 14:38:31 by audgiova         ###   ########.fr       */
+/*   Updated: 2026/08/21 15:09:09 by audgiova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,17 +71,16 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (!s2)
 		return (NULL);
 	if (!s1)
-	{
 		len_s1 = 0;
-	}
 	else
-	{
 		len_s1 = ft_strlen((char *)s1);
-	}
 	len_s2 = ft_strlen((char *)s2);
-	buffer = malloc(len_s1 + len_s2 + 1);
+	buffer = (char *)malloc(len_s1 + len_s2 + 1);
+	if (!buffer)
+		return (NULL);
 	ft_memcpy(buffer, s1, len_s1);
 	ft_memcpy(buffer + len_s1, s2, len_s2);
-	free(s1);
+	buffer[len_s1 + len_s2] = '\0';
+	free((void *)s1);
 	return (buffer);
 }
