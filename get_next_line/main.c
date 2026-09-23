c960fba5cb7baaed33d9652ac24674ade2df157f
Audy <audgiova@student.42sp.org.br>
2026-09-23 15:43:54 -0300

    submit 00_push_swap

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,32 +6,27 @@
 /*   By: audgiova <audgiova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/21 17:00:43 by audgiova          #+#    #+#             */
-/*   Updated: 2026/08/21 17:42:25 by audgiova         ###   ########.fr       */
+/*   Updated: 2026/09/19 17:02:41 by audgiova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
-#include "get_next_line.h"
 
-int	main(void)
+int main(void)
 {
-	int		fd;
-	char	*line;
+    int     fd;
+    char    *line;
 
-	fd = open("vazio.txt", O_RDONLY);
-	if (fd < 0)
-	{
-		printf("Erro ao abrir o arquivo!\n");
-		return (1);
-	}
-	line = get_next_line(0);
-	while (line != NULL)
-	{
-		printf("%s", line);
-		free(line);
-		line = get_next_line(0);
-	}
-	close(fd);
-	return (0);
+    fd = open("README.md", O_RDONLY);
+    if (fd < 0)
+        return (1);
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("%s", line);
+        free(line);
+    }
+    close(fd);
+    return (0);
 }
